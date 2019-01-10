e9586605eadea0ea57f437eaaf3e08a3e0732b94
Ali Korkmaz <ali.korkmaz@ibtech.com.tr>
2019-01-10 13:19:34 +0300

    initial2

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import App from "components/App";
+import { shallow, unmount } from "enzyme";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+afterEach(() => {
+  wrapped.unmount(); // not sure
+});
+
+it("shows a comment box", () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
